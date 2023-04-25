9c913f18ef5cbf286949287b46e530561d901a77
Vincent Koc <vincentkoc@ieee.org>
2023-04-25 15:01:15 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="no-weekend-merge",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "PyGithub==1.55",
+        "pytz==2021.3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "no-out-of-hours-merge = no-out-of-hours-merge:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
