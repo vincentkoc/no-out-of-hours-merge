1572887571fa4306c5a4e3013a18ca8c337a8c9a
Vincent Koc <vincentkoc@ieee.org>
2023-04-25 18:12:22 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-# export GITHUB_TOKEN="$1"
-# export CI_PR_TITLE="$2"
-# export TIMEZONE="$3"
-# export RESTRICTED_TIMES="$4"
-# export CUSTOM_MESSAGE="$5"
-# export CHECK_EXISTING_COMMENT="$6"
+export GITHUB_TOKEN="$1"
+export CI_PR_TITLE="$2"
+export TIMEZONE="$3"
+export RESTRICTED_TIMES="$4"
+export CUSTOM_MESSAGE="$5"
+export CHECK_EXISTING_COMMENT="$6"
 
-# if [ -z "$GITHUB_TOKEN" ]; then
-#     echo "GITHUB_TOKEN environment variable is not set"
-#     exit 1
-# fi
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN environment variable is not set"
+    exit 1
+fi
 
 python3 /src/main.py
 
