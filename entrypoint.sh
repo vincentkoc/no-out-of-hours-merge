18f7ea9f75c67051c21c48e69e84a50d676f0963
Vincent Koc <vincentkoc@ieee.org>
2023-04-25 16:53:45 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-export GITHUB_TOKEN="$1"
-export CI_PR_TITLE="$2"
-export TIMEZONE="$3"
-export RESTRICTED_TIMES="$4"
-export CUSTOM_MESSAGE="$5"
-export CHECK_EXISTING_COMMENT="$6"
-
+# export GITHUB_TOKEN="$1"
+# export CI_PR_TITLE="$2"
+# export TIMEZONE="$3"
+# export RESTRICTED_TIMES="$4"
+# export CUSTOM_MESSAGE="$5"
+# export CHECK_EXISTING_COMMENT="$6"
 
 python3 /src/main.py
 
