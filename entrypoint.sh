c599d3b2a89e9e5c870ce97f40317003dacd5dba
Vincent Koc <vincentkoc@ieee.org>
2023-04-25 14:47:47 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,10 @@ set -e
 
 export GITHUB_TOKEN="$1"
 export CI_PR_TITLE="$2"
+export TIMEZONE="$3"
+export RESTRICTED_TIMES="$4"
+export CUSTOM_MESSAGE="$5"
 
-# Check current time and block merge during specified times
 python3 /check_merge_time.py
 
 echo "Done"
