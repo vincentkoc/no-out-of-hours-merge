491e093d37e1ca4def7f612123551f88d4d0f47f
Vincent Koc <vincentkoc@ieee.org>
2023-04-25 15:06:09 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
